a006cc668594fe9651969fb8269b135afc360367
trunganh98 <trunganh98@users.noreply.github.com>
2020-08-08 12:05:37 +0700

    .

--- resources/views/editTask.blade.php
+++ resources/views/editTask.blade.php
@@ -1,13 +1,15 @@
 @extends('user_layout')
 @section('content')
-                @foreach($tasks as $p)
-                <form action="{{URL::to('update_task/'.$p->id)}}" method="post">
-                    {{csrf_field()}}
-                    Name<input type="text" class="form-control" name="name" value="{{$p->name}}" required=""><br>
-                    Price<input type="text" class="form-control" name="price" value="{{$p->price}}" required=""><br>
-                    Description<input type="text" class="form-control" name="description" value="{{$p->description}}" required=""><br>
-                    <div class="clearfix"></div>
-                    <input type="submit" class="btn btn-success" value="Update Task" name="update_task" data-target="#exampleModal">
-                </form> <br><br>
-                @endforeach
+    <h1>Update Task</h1>
+    @foreach($tasks as $p)
+        <form action="{{URL::to('update_task/'.$p->id)}}" method="post">
+            {{csrf_field()}}
+            Name<input type="text" class="form-control" name="name" value="{{$p->name}}" required=""><br>
+            Price<input type="text" class="form-control" name="price" value="{{$p->price}}" required=""><br>
+            Description<input type="text" class="form-control" name="description" value="{{$p->description}}" required=""><br>
+            <div class="clearfix"></div>
+            <input type="submit" class="btn btn-success" value="Update Task" name="update_task" data-target="#exampleModal">
+        </form> <br><br>
+    @endforeach
+
 @endsection
